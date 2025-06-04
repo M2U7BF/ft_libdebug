3399fb3bc1255b21f875cb4b50f930c0c6513051
M2U7BF <yhvh4321@gmail.com>
2025-06-04 11:30:35 +0900

    update

--- libdebug.h
+++ libdebug.h
@@ -6,7 +6,7 @@
 /*   By: kkamei <kkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 16:48:45 by kkamei            #+#    #+#             */
-/*   Updated: 2025/06/04 08:34:58 by kkamei           ###   ########.fr       */
+/*   Updated: 2025/06/04 11:22:58 by kkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define DEBUG_H
 
 # include <stdio.h>
+# include <string.h>
 
+// put_debug.c
 void	put_strarr(char **str);
+int		put_strarr_compare(char **str, char **exp);
 
 #endif
